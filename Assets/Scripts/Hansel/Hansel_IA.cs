ede3ec8bfa0d7293908d783ba68a5be9686bd3c2
sayats16 <sayats16@gmail.com>
2024-03-27 20:47:02 +0100

    Trying projectile throw shananigans

--- Assets/Scripts/Hansel/Hansel_IA.cs
+++ Assets/Scripts/Hansel/Hansel_IA.cs
@@ -1,33 +1,53 @@
 using Opsive.UltimateCharacterController.Demo.AI;
+using Opsive.UltimateCharacterController.Items.Actions.Impact;
+using Opsive.UltimateCharacterController.Objects;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements;
+using static UnityEngine.GraphicsBuffer;
 
 public class Hansel_IA : MonoBehaviour
 {
 
-    //hansel vars
+    //hansel general vars
     public bool alive;
+    public float throwForce = 200f;
+    public float throwForceUp = 1f; //forca que efectua al tirar lobjecte perque vagi cap amunt
+    private float intervalBT = 0.5f; //segons que pasen entre un check del BT i el seguent
+    private float btTimer = 0f;
+
+
+    //apples
+    public GameObject pApple;
+    public float pickupDistance = 1.3f;
     public int apples;
-    public float appleRadius=20f;
-    public float pickupDistance=1.3f;
+    public float appleRadius = 20f;
+    public int maxApples=5;
+    private GameObject gThrowPoint;
+
+    //ballistica
+    public float firingAngle = 45.0f;
+    public float gravity = 9.8f;
+
 
     //navmesh
     private NavMeshAgent agent;
     public float followDistance=3.5f; //a partir daquesta distancia comença a seguir el jugador
     private float stopDistanceFollow = 2f; //ens parem a 2f de la Gretel
-
+    public float healDistance = 6f; //a partir daquesta distancia pot tirar la poma a la Gretel
 
     //gretel vars
     public GameObject gGretel;
     public int gretelLife;
     private int gretelNeedsHeal=5; // numero de vides en el qual haurem de curarla
-    
+    private float gretelDistance;
+
 
     //knockout vars
-    private float gretelDistance;
     private float resurrectionDistance=1.3f;
     private float abandoneDistance = 30f;
 
@@ -35,13 +55,30 @@ public class Hansel_IA : MonoBehaviour
     void Start()
     {
         alive = true;
-        gretelLife = 10; //testing (asigna)
+        gretelLife = 1; //testing (asigna)
         agent = GetComponent<NavMeshAgent>();
+        gThrowPoint = transform.GetChild(0).gameObject;
+
+        if (gGretel == null)
+        {
+            Debug.Log("falta asignar la Gretel");
+        }
+        if(pApple == null)
+        {
+            Debug.Log("falta asignar el prefab de la poma");
+        }
     }
 
     void Update()
     {
-        decisionTree();
+        btTimer += Time.deltaTime;
+        //executem cada X segons
+        if (btTimer > intervalBT)
+        {
+            decisionTree();
+            btTimer = 0f;
+        }
+        
     }
 
 
@@ -95,6 +132,15 @@ public class Hansel_IA : MonoBehaviour
                     //no hi han pomes aprop
                     Debug.Log("no hi ha pomes aprop i en necessito, follow gretel");
                     followGretel();
+                    return;
+                }
+
+                //tinc pomes, curo a la Gretel
+                else
+                {
+                    healGretel();
+                    
+
                 }
             }
 
@@ -115,7 +161,7 @@ public class Hansel_IA : MonoBehaviour
         {
             if (obj.GetComponent<Apple>())
             {
-                apples++;
+                updateApples(1);
                 obj.GetComponent<Apple>().picked();
             }
         }
@@ -124,14 +170,14 @@ public class Hansel_IA : MonoBehaviour
             //ens acostem al objecte
             Debug.Log("going to pickup object");
             agent.destination = obj.transform.position;
-
+            agent.stoppingDistance = 0f;
 
         }
 
     }
 
     //segueix a la Gretel quan esta mes lluny de la followDistance
-    public void followGretel()
+    private void followGretel()
     {
         if (gGretel!=null)
         {
@@ -148,6 +194,71 @@ public class Hansel_IA : MonoBehaviour
         }
         
     }
+
+    //a partir de X distancia de la Gretel, li pot llencar la poma per curarla. Sino si acosta
+    private void healGretel()
+    {
+        if(gGretel!=null)
+        {
+            gretelDistance = Vector3.Distance(transform.position, gGretel.transform.position);
+
+            //ens apropem
+            if( gretelDistance > healDistance)
+            {
+                Debug.Log("ens aprop per curar");
+                agent.destination= gGretel.transform.position;
+                agent.stoppingDistance = healDistance;
+            }
+            //tirem la poma si tenim linia de visio amb la Gretel
+            else
+            {
+                Debug.Log("hauriem de tirar la poma!");
+
+                //mira cap a la Gretel
+                transform.LookAt(gGretel.transform);
+
+                //la direccio entre A i B =  (B - A)
+                Vector3 directionGretel = gGretel.transform.position - gThrowPoint.transform.position;
+
+                //tirem un raycast per mirar si no hi ha res entre mitg que impedeixi tirar la poma
+                RaycastHit hit;
+
+                if(Physics.Raycast(gThrowPoint.transform.position, directionGretel, out hit, healDistance))
+                {
+                    Debug.DrawRay(gThrowPoint.transform.position, directionGretel, Color.blue, 2f);
+                    Debug.Log("Line Of Vision with Gretel");
+
+                    if(hit.transform.tag == "Gretel")
+                    {
+                        Debug.Log("throw apple");
+                        //instancio la poma i la llenco amb un add force
+                        Transform projectil = Instantiate(pApple, gThrowPoint.transform.position, gThrowPoint.transform.rotation).transform;
+                        projectil.GetComponent<Apple>().throwApple(gGretel.transform);
+                        //Rigidbody rbProjectil = projectil.GetComponent<Rigidbody>();
+                        //Vector3 force = directionGretel.normalized * throwForce + transform.up * throwForceUp;
+                        //rbProjectil.AddForce(force);
+
+                        updateApples(-1);
+                    }
+
+                    
+
+                }
+
+               
+            }
+
+        }
+    }
+
+    //suma i resta fins arribar als limits
+    private void updateApples(int quantity)
+    {
+        apples += quantity;
+        Mathf.Clamp(apples, 0, maxApples); 
+    }
+
+     
 
 
     //reviu en Hansel
--- Assets/Scripts/Objects/Apple.cs
+++ Assets/Scripts/Objects/Apple.cs
@@ -1,16 +1,37 @@
+using Opsive.UltimateCharacterController.Objects;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Apple : MonoBehaviour
 {
-    public bool collectable=false;
+    private bool collectable=false;
+    private bool from_hansel = false;
+
+
+    //ballistica
+    public float speed = 8.5f; // Speed of projectile.
+    public float radius = 1f; // Collision radius.
+    float radiusSq; // Radius squared; optimisation.
+    public Transform target; // Who we are homing at.
+
+    Vector3 currentPosition; // Store the current position we are at.
+    float distanceTravelled; // Record the distance travelled.
+
+    public float arcFactor = 0.5f; // Higher number means bigger arc.
+    Vector3 origin; // To store where the projectile first spawned.
 
 
     // Quan li tirin un projectil caura
     private void OnCollisionEnter(Collision collision)
     {
-        collectable = true;   
+        collectable = true;
+        if (collision.gameObject.tag == "Gretel")
+        {
+            //crida la funcio de curacio de Gretel
+            //collision.gameObject.GetComponent<Test>().heal();
+            Destroy(gameObject);
+        }
     }
     
 
@@ -22,5 +43,49 @@ public class Apple : MonoBehaviour
     {
         Debug.Log("apple picked up!");
         Destroy(gameObject);
+    }
+
+    public void setFromHansel(bool c)
+    {
+        from_hansel=c;
+    }
+
+    public bool isFromHansel()
+    {
+        return from_hansel;
+    }
+
+
+
+    //ballistica
+    void Update()
+    {
+        // Nomes farem el calcul de ballistica quan el llencem
+        if (target!=null)
+        {
+            Debug.Log("trhooooow");
+
+            // Move ourselves towards the target at every frame.
+            Vector3 direction = target.position - currentPosition;
+            currentPosition += direction.normalized * speed * Time.deltaTime;
+            distanceTravelled += speed * Time.deltaTime; // Record the distance we are travelling.
+
+            // Set our position to <currentPosition>, and add a height offset to it.
+            float totalDistance = Vector3.Distance(origin, target.position);
+            float heightOffset = arcFactor * totalDistance * Mathf.Sin(distanceTravelled * Mathf.PI / totalDistance);
+            transform.position = currentPosition + new Vector3(0, 0, heightOffset);
+ 
+
+        }
+
+        
+    }
+
+    // So that other scripts can use Projectile.Spawn to spawn a projectile.
+    public void throwApple(Transform targetEntered)
+    {
+        radiusSq = radius * radius;
+        origin = transform.position;
+        target = targetEntered;
     }
 }
